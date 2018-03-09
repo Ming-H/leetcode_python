3cf68998624f4243c27ff3ada97f55dfc0a4c078
Ming <1518246548@qq.com>
2018-03-09 16:47:56 +0800

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -12,27 +12,26 @@ return [0, 1].
 """
 
 
-def twoSum(nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    L = []
-    for i in range(0, len(nums)-1):
-        print('*******************','\n',nums[i])
-        for j in (i+1, len(nums)-1):
-            print(nums[j])
-            # if (nums[i] + nums[j]) == target:
-                # print(i,j)
-                # if i not in L:
-                    # L.append(i)
-                # if j not in L:
-                    # L.append(j)
-    return L
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        self.nums = nums
+        self.target = target
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    print(i,j)
+        return i,j
+
+
     
 if __name__ == "__main__":
     nums = [-3,4,3,90]
     target = 0
-    print(twoSum(nums, target))
+    ss = Solution()
+    ss.twoSum(nums, target)
             
