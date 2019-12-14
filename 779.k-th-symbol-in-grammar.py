7dfa02b224533c3bad22c5370d66c3c3b0e2028d
Ming <1518246548@qq.com>
2019-12-14 10:51:35 +0800

    Update 779.k-th-symbol-in-grammar.py

--- 779.k-th-symbol-in-grammar.py
+++ 779.k-th-symbol-in-grammar.py
@@ -7,9 +7,20 @@
 # @lc code=start
 class Solution:
     def kthGrammar(self, N: int, K: int) -> int:
-        mapping = ((0, 1), (1, 0))
-        if N == 1:
-            return 0
-        return mapping[self.kthGrammar(N - 1, K // 2 + K % 2)][(K - 1) % 2]
+        """
+        Time complexity: O(N)
+        Space complexity: O(1)
+        可以看作异或
+        """
+        ans = 0
+        s, e = 1, 2**(N-1)
+        while s<e:
+            m = s+(e-s)//2
+            if m < K:
+                ans = ans^1 
+                s = m+1
+            else:
+                e = m
+        return ans
 # @lc code=end
 
