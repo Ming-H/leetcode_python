ca96519e2d48f7e8c7feb1c625401fb51cfe7204
Ming-H <1518246548@qq.com>
2020-06-14 11:49:35 +0800

    226

--- 226.invert-binary-tree.py
+++ 226.invert-binary-tree.py
@@ -12,7 +12,47 @@
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
-        if root:
-            root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        # if root:
+        #     root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+        # return root
+
+        # if not root:
+        #     return None
+        # left = self.invertTree(root.left)
+        # right = self.invertTree(root.right)
+        # root.left, root.right = right, left
+        # return root
+
+        if not root:
+            return None
+        queue = [root]
+        while queue:
+            current = queue.pop(0)
+            tmp = current.left
+            current.left = current.right
+            current.right = tmp
+            if current.left :
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
         return root
+
+
+        # if (root == null) return null;
+        #     Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        #     queue.add(root);
+        #     while (!queue.isEmpty()) {
+        #         TreeNode current = queue.poll();
+        #         TreeNode temp = current.left;
+        #         current.left = current.right;
+        #         current.right = temp;
+        #         if (current.left != null) queue.add(current.left);
+        #         if (current.right != null) queue.add(current.right);
+        #     }
+        #     return root;
+
+            
+
+
+
 
